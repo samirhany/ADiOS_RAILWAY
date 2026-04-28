f819d9db393c8359fb9c6131bea5164fe1d65257
Samir Hany <samirMAKHRABAN@GMAIL.com>
2026-04-28 15:24:59 +0200

    Fix DB env variables

--- backend/ADiOS-backend/db.js
+++ backend/ADiOS-backend/db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql2');
 
 // استخدام Pool لضمان استقرار الاتصال
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 
 // اختبار الاتصال
 db.getConnection((err, connection) => {
